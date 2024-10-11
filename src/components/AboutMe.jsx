9ae12fc789e871d10850485570c7ee8162aa2aa3
esther57-web <kamardine.ossete@gmail.com>
2024-10-11 13:00:32 +0200

    about responsive computer done

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -6,19 +6,19 @@ import Cta from './Cta.jsx';
 const AboutMe = () => {
     const { theme } = useTheme();
   return (
-    <section className={`flex justify-center ${theme === "dark" ? "dark" : "light"}`}>
-        <div className='flex xs:flex-col items-center'>
-            <div className='flex justify-center'>
-                <img className='rounded-full xs:w-[18rem] xs:h-[18rem]' src={theme === "dark" ? darkAbout : lightAbout} alt='image d&apos;un ordinateur'></img>
+    <section className={`flex justify-center ${theme === "dark" ? "dark" : "light"} lg:py-16`}>
+        <div className='flex xs:flex-col xs:items-center lg:flex-row lg:w-[80%] lg:justify-center'>
+            <div className='flex justify-center lg:w-[50%]'>
+                <img className='rounded-full xs:w-[18rem] xs:h-[18rem] lg:w-[24rem] lg:h-[24rem]' src={theme === "dark" ? darkAbout : lightAbout} alt='image d&apos;un ordinateur'></img>
             </div>
-            <div className='xs:w-[80%] flex xs:flex-col xs:gap-4 xs:relative xs:bottom-16'>
-                <h2>A propos <br className='xs:hidden'/>de moi</h2>
-                <p className='xs:text-center xs:text-xs sm:text-sm'>Développeuse web full stack passionnée. Je suis motivée, autonome et organisée,
+            <div className='xs:w-[80%] lg:w-[50%] flex xs:flex-col xs:gap-4 xs:relative xs:bottom-16 md:gap-6'>
+                <h2 className='lg:text-start'>A propos <br className='xs:hidden'/>de moi</h2>
+                <p className='xs:text-center lg:text-start xs:text-xs sm:text-sm'>Développeuse web full stack passionnée. Je suis motivée, autonome et organisée,
                  ce qui me permet de travailler efficacement en full remote. En raison de mon handicap, 
                  je recherche des opportunités qui valorisent la flexibilité et l&apos;inclusion. 
                  Je suis impatiente de mettre mes compétences en développement à profit et de contribuer 
                  à des projets innovants.</p>
-                 <div className='flex justify-center'>
+                 <div className='flex xs:justify-center lg:justify-start'>
                     <Cta content='Embauchez-moi'/>
                  </div>
                 
